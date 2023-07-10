2be5317f3d2d60d00600b2166aca031c4cf50e5c
rubenkarapetyan123 <ruben.karapetyan2@tumo.org>
2023-07-10 18:58:12 +0400

    header structure

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+import "./Header.css"
+import { LOGIN, REGISTER } from "../../constants/routes-constants"
+
+
 function Header(){
   return (
-    <p>header</p>
+    <header>
+      <div>Logo</div>
+      <Link to={LOGIN}>Login</Link>
+      <Link to={REGISTER}>Register</Link>
+    </header>
   )
 }
 
