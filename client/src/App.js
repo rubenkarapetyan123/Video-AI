98d6f5df10910549ac4e9f05ac96af791cebd64f
rubenkarapetyan123 <ruben.karapetyan2@tumo.org>
2023-07-10 18:46:54 +0400

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import './App.css';
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Router from "./routing/Router";
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Header/>
       <Router/>
+      <Footer/>
     </BrowserRouter>
   );
 }
