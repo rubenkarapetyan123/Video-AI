b6db5251c75dc7001effca9e4e06dbe57b181ddb
rubenkarapetyan123 <ruben.karapetyan2@tumo.org>
2023-07-10 18:40:34 +0400

    login reg and main routes

--- client/src/routing/Router.js
+++ client/src/routing/Router.js
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router"
-
+import Login from "../components/pages/Login/Login"
+import Main from "../components/pages/Main/Main"
+import Register from "../components/pages/Register/Register"
+import { LOGIN, REGISTER, MAIN } from "../constants/routes-constants"
 
 function Router(){
   return (
     <Routes>
-      <Route path="*" element={<p>hdsFbhgfsbhjvdfhjbfvsbjhf</p>}/>
+      <Route path={LOGIN} element={<Login/>}/>
+      <Route path={REGISTER} element={<Register/>}/>
+      <Route path={MAIN} element={<Main/>}/>
     </Routes>
   )
 }
