fdbc7aee822fef71eb5ac5c37320768613848f01
rubenkarapetyan123 <ruben.karapetyan2@tumo.org>
2023-07-10 18:43:19 +0400

    add header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom"
 import './App.css';
+import Header from "./components/header/Header";
 import Router from "./routing/Router";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Router/>
     </BrowserRouter>
   );
