d39e2b39e0f37cccf9f083f8d98f4242dc6b734a
git config --global user.name <ruben.karapetyan2@tumo.org>
2023-07-11 17:03:13 +0400

    login inputs controls

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -1,19 +1,36 @@
+import { useState } from "react"
 import FormButton from "../../usable/FormButton"
 import FormInput from "../../usable/FormInput"
 
 function Login(){
+  const [inputData,setInputData] = useState({
+    email : "",
+    password : ""
+  })
+  const emailHandle = e => setInputData({
+    ...inputData,email : e.target.value})
+  const passwordHandle = e => setInputData({
+    ...inputData,password : e.target.value})
+
+
   return (
     <div className="reg-container">
         <h1>Login</h1>
-        <FormInput
-          type={"email"}
-        />
-        <FormInput
-          type={"password"}
-        />
-        <FormButton
-          text={"Sign in"}
-        />
+        <form>
+          <FormInput
+            type={"email"}
+            value={inputData.email}
+            handle={emailHandle}
+          />
+          <FormInput
+            type={"password"}
+            value={inputData.password}
+            handle={passwordHandle}
+          />
+          <FormButton
+            text={"Sign in"}
+          />
+        </form>
     </div>
   )
 }
