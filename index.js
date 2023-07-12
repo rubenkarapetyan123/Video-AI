383f3ee2c4ced2b51ec4c7fb0980ae1c790cc1e9
git config --global user.name <ruben.karapetyan2@tumo.org>
2023-07-12 16:48:45 +0400

    register system

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import { LOGIN, REGISTER } from "./constants/routes-constants.js";
+import fs from "fs"
+import { nanoid } from "nanoid";
+import bcrypt from "bcrypt"
+import { log } from "console";
 
 const app = express()
 app.use(express.urlencoded({extended : true}))
@@ -13,11 +17,36 @@ app.get("/test",(req,res)=>{
 })
 
 
-app.post(REGISTER,(req,res)=>{
+app.post(REGISTER,async (req,res)=>{
   const { username, email, password } = req.body
-  console.log(username, email, password);
+  const users = JSON.parse(fs.readFileSync("./database/users.json",{encoding : "utf8",flag : "r"}))
+  let isValidEmail = true
+  for(let i in users){
+    if(users[i].email === email){
+      isValidEmail = false
+    }
+  }
+  if(!isValidEmail){
+    return res.send({
+      access : false,
+      message : "email already used"
+    })
+  }
+
+  const id = nanoid(8)
+  users[id] = {
+    username,
+    email,
+    password : await bcrypt.hash(password,10)
+  }
+
+  fs.writeFileSync("./database/users.json",JSON.stringify(users,undefined,2))
+
   res.send({
-    access : true
+    access : true,
+    username,
+    email,
+    id
   })
 })
 
